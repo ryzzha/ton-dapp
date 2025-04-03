eb093198d97f3e8392cd19f3402d635c16ebb6e3
IraRyzha <ira.ryzha2803@gmail.com>
2025-04-03 21:20:34 +0300

    push contract directory

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  define: {
+    global: 'globalThis', // Робить global доступним, як у Node.js
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer', // Використовуємо npm-пакет buffer
+    },
+  },
 })
